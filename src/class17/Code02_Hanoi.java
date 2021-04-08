4ba6545b3d8399cd9cc47c79842cd863b6a6e0c7
ligang <ligangag@yonyou.com>
2021-04-08 09:30:26 +0800

    汉诺塔问题

--- src/class17/Code02_Hanoi.java
+++ src/class17/Code02_Hanoi.java
@@ -7,9 +7,19 @@ import java.util.Stack;
  * 汉诺塔问题
  * <p>
  * 暴力递归
+ * <p>
+ * 思路：
+ * <p>
+ * 1、1～N-1 左--》中  大步
+ * 2、N  左-》右
+ * 3、1～N-1 中--》右  大步
  */
 public class Code02_Hanoi {
-
+    /**
+     * 下面的6和方法就是滕路的过程
+     *
+     * @param n
+     */
     public static void hanoi1(int n) {
         leftToRight(n);
     }
@@ -20,7 +30,9 @@ public class Code02_Hanoi {
             System.out.println("Move 1 from left to right");
             return;
         }
-
+        /**
+         * 下面两个方法领域理解成黑盒
+         */
         leftToMid(n - 1);
         System.out.println("Move " + n + " from left to right");
         midToRight(n - 1);
@@ -77,19 +89,53 @@ public class Code02_Hanoi {
         midToLeft(n - 1);
     }
 
+
+    /**
+     * from--》to  other    忘掉左中右
+     * <p>
+     * 1、1～N-1  from-->other
+     * 2、N       from-->to
+     * 3、1～N-1  other-->to
+     * <p>
+     * <p>
+     * 整个问题  N  左--》右
+     * <p>
+     * 拆分后的一个子问题   N-1 左--》中
+     * <p>
+     * 左--右
+     * 左--中
+     * <p>
+     * 中--右
+     * 中--左
+     * <p>
+     * 右--左
+     * 右--中
+     * 当N非常大的时候，上面的这六个步骤肯定都会出现
+     * 所以第一种方法考虑这6中嵌套
+     * <p>
+     * 方法2
+     * 就是加了几个参数，代表了6个过程
+     */
     public static void hanoi2(int n) {
         if (n > 0) {
+
             func(n, "left", "right", "mid");
         }
     }
 
     public static void func(int N, String from, String to, String other) {
+        int i = 0;
+        //这里N是停止打印的标志
         if (N == 1) { // base
             System.out.println("Move 1 from " + from + " to " + to);
         } else {
+            //N-1 从from --》to  目的是把N-1从from移动到 other，，，所以to变成了other
+
             func(N - 1, from, other, to);
             System.out.println("Move " + N + " from " + from + " to " + to);
             func(N - 1, other, to, from);
+            i++;
+            System.out.println(i);
         }
     }
 
@@ -108,6 +154,7 @@ public class Code02_Hanoi {
             other = o;
         }
     }
+
 
     public static void hanoi3(int N) {
         if (N < 1) {
@@ -135,11 +182,11 @@ public class Code02_Hanoi {
     }
 
     public static void main(String[] args) {
-        int n = 3;
-        hanoi1(n);
+        int n = 4;
+//        hanoi1(n);
         System.out.println("============");
         hanoi2(n);
-//		System.out.println("============");
+        System.out.println("============");
 //		hanoi3(n);
     }
 
