37031084e85f0df6778091c3fa43be7aa4c47879
ligang <ligangag@yonyou.com>
2021-04-09 14:30:02 +0800

    栈逆序 不加数据结构 递归函数实现 栈实现数据的临时记录

--- src/class17/Code05_ReverseStackUsingRecursive.java
+++ src/class17/Code05_ReverseStackUsingRecursive.java
@@ -15,7 +15,14 @@ import java.util.Stack;
  */
 
 public class Code05_ReverseStackUsingRecursive {
-
+    /**
+     * 每一次都返回最底层的
+     * <p>
+     * 这个返回的也是相当于逆序了，，把底层的记录下来了
+     * 之后在返回的时候，压栈，，达到了逆序的目的
+     *
+     * @param stack
+     */
     public static void reverse(Stack<Integer> stack) {
         if (stack.isEmpty()) {
             return;
@@ -25,9 +32,21 @@ public class Code05_ReverseStackUsingRecursive {
         stack.push(i);
     }
 
-    // 栈底元素移除掉
-    // 上面的元素盖下来
-    // 返回移除掉的栈底元素
+    /**
+     * 栈底元素移除掉
+     * 上面的元素盖下来
+     * 返回移除掉的栈底元素
+     * <p>
+     * 每一次返回的都是last
+     * result是暂时存储的数据
+     * |1|
+     * |2| -函数->  |1|
+     * |3|			|2|
+     * <p>
+     * 每次执行之后，最底层值去掉，并返回
+     * result是临时存储的信息（每次递归保存）
+     * 等下次调用结束之后，在压栈
+     */
     public static int f(Stack<Integer> stack) {
         int result = stack.pop();
         if (stack.isEmpty()) {
@@ -41,11 +60,12 @@ public class Code05_ReverseStackUsingRecursive {
 
     public static void main(String[] args) {
         Stack<Integer> test = new Stack<Integer>();
-        test.push(1);
-        test.push(2);
         test.push(3);
-        test.push(4);
-        test.push(5);
+        test.push(2);
+        test.push(1);
+
+//        test.push(4);
+//        test.push(5);
         reverse(test);
         while (!test.isEmpty()) {
             System.out.println(test.pop());
