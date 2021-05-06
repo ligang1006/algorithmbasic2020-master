0b2945d3440f8509cee909888aa842e6747be1f1
ligang <ligangag@yonyou.com>
2021-05-06 10:10:37 +0800

    图的深度和宽度优先遍历

--- src/class16/Code01_BFS.java
+++ src/class16/Code01_BFS.java
@@ -4,27 +4,40 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
 
+/**
+ * 图的宽度和深度优先遍历
+ * <p>
+ * 宽度：
+ * 1、利用队列实现
+ * 2、从源节点开始按照宽度进队列，然后弹出
+ * 3、每弹出一个节点，把该节点所有没有进队列的邻节点放入队列
+ * 4、直到队列变空
+ * 在二叉树中用的队列
+ *
+ */
 public class Code01_BFS {
 
-	// 从node出发，进行宽度优先遍历
-	public static void bfs(Node start) {
-		if (start == null) {
-			return;
-		}
-		Queue<Node> queue = new LinkedList<>();
-		HashSet<Node> set = new HashSet<>();
-		queue.add(start);
-		set.add(start);
-		while (!queue.isEmpty()) {
-			Node cur = queue.poll();
-			System.out.println(cur.value);
-			for (Node next : cur.nexts) {
-				if (!set.contains(next)) {
-					set.add(next);
-					queue.add(next);
-				}
-			}
-		}
-	}
+    // 从node出发，进行宽度优先遍历
+    public static void bfs(Node start) {
+        if (start == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        //二叉树遍历没有环的问题，不会多次进入队列，set的作用是防止产生环，使程序执行完成
+        HashSet<Node> set = new HashSet<>();
+        queue.add(start);
+        set.add(start);
+        //只要有节点，就把他的邻节点加进去
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            System.out.println(cur.value);
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    set.add(next);
+                    queue.add(next);
+                }
+            }
+        }
+    }
 
 }
--- src/class16/Code02_DFS.java
+++ src/class16/Code02_DFS.java
@@ -3,32 +3,48 @@ package class16;
 import java.util.HashSet;
 import java.util.Stack;
 
+/**
+ * 深度优先遍历：
+ * 一条路走到底，再往上返回
+ * <p>
+ * 1、栈实现
+ * 2、从源节点开始，把节点按照深度放入栈，然后弹出
+ * 3、每弹出一个节点，把该节点下一个没有进过栈的邻接点放入栈
+ * 4、直到栈变空
+ */
 public class Code02_DFS {
 
-	public static void dfs(Node node) {
-		if (node == null) {
-			return;
-		}
-		Stack<Node> stack = new Stack<>();
-		HashSet<Node> set = new HashSet<>();
-		stack.add(node);
-		set.add(node);
-		System.out.println(node.value);
-		while (!stack.isEmpty()) {
-			Node cur = stack.pop();
-			for (Node next : cur.nexts) {
-				if (!set.contains(next)) {
-					stack.push(cur);
-					stack.push(next);
-					set.add(next);
-					System.out.println(next.value);
-					break;
-				}
-			}
-		}
-	}
-	
-	
-	
+    public static void dfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        //栈记录的是深度优先遍历的路径
+        Stack<Node> stack = new Stack<>();
+        //防止重复，来回走
+        HashSet<Node> set = new HashSet<>();
+        stack.add(node);
+        set.add(node);
+        //进栈的时候打印
+        System.out.println(node.value);
+
+        while (!stack.isEmpty()) {
+            Node cur = stack.pop();
+            //枚举当前节点的后代
+            for (Node next : cur.nexts) {
+                if (!set.contains(next)) {
+                    //把当前的和下一个压进去
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    System.out.println(next.value);
+                    //a 的直接有bcd ，当把b和a再压入栈之后，break后就不会打印了
+
+                    //之后就不在走了，其他的邻居在以后的时间在进入
+                    break;
+                }
+            }
+        }
+    }
+
 
 }
